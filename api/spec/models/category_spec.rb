eb1d35119937e75a100f281066c2de293e8fbdc3
Marcos Ganine <marcosganine@gmail.com>
2022-11-27 23:28:07 -0300

    Create Categories resource

---
+++ api/spec/models/category_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+
+  it 'has a valid factory' do
+    expect(build(:category)).to be_valid
+  end
+end
