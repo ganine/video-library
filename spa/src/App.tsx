66ff03028d00068a87a616196d9fe5cab5fe27e3
Marcos Ganine <marcosganine@gmail.com>
2022-11-29 23:22:18 -0300

    Setup Bootstrap CSS

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -7,14 +7,25 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div className="app">
+      <header className="app-header">
         <h1>Video Library</h1>
-        <Routes>
-          <Route path="/upload" element={<UploadPage />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+      </header>
+
+      <div className="container-md text-center">
+
+        <div className="row app-content">
+          <div className="col">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/upload" element={<UploadPage />} />
+                <Route path="/" element={<HomePage />} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
